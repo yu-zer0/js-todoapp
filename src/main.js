84678a5e1e5f3851f27728d6ad52c8a534ca3676
yu-zer0 <bluette96@icloud.com>
2025-10-26 21:20:29 +0900

    アロー関数について

--- src/main.js
+++ src/main.js
@@ -50,8 +50,43 @@
 // console.log(val5);
 
 // テンプレート文字列
-const name = "ももたろう";
-const age = 25;
-const message = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message);
+// const name = "ももたろう";
+// const age = 25;
+// const message = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message);
+
+// アロー関数
+// function add(x, y) {
+//   return x+y;
+// };
+// 関数を一度変数に渡すこともできる
+// const add = function(x, y) {
+//   return x+y;
+// };
+
+// console.log(add(5,3));
+// function hello(str) {
+//   console.log(str);
+// }
+// hello("hello");
+
+// アロー関数
+const arrowfunc = (x,y) => {
+  return console.log(x+y);
+};
+arrowfunc(5,3);
+
+// 引数が１つのときは（）を省略できる
+const echoMessage = str => {
+  return console.log(str);
+};
+echoMessage("Hello, World");
+
+// Reactでよく使われる。オブジェクトを返すときの書き方
+const func = (num1, num2) => ({
+  hoge: num1,
+  fuga: num2
+});
+
+console.log(func(10,30));
 
