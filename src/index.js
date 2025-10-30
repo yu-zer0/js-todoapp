d6a164248135273e19316eb4496148e2bd7394d7
yu-zer0 <bluette96@icloud.com>
2025-10-30 23:12:12 +0900

    タスクの完了削除ボタン機能を追加

--- src/index.js
+++ src/index.js
@@ -16,9 +16,42 @@ const onClickAdd = () => {
     p.className = "todo-item";
     p.innerText = inputText;
 
+    // button(完了)タグ生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => {
+        // 以下のコメントは削除機能を参考に自作（習熟をかねて）
+        // const completeTarget = completeButton.closest("li");
+        // const completeTargetDiv = completeButton.closest("div");
+        // completeTargetDiv.removeChild(completeButton);
+        // completeTargetDiv.removeChild(deleteButton);
+        // const backButton = document.createElement("button");
+        // backButton.innerText = "戻す";
+        // completeTargetDiv.appendChild(backButton);
+        // document.getElementById("complete-list").appendChild(completeTarget);
+        const moveTarget = completeButton.closest("li");
+        completeButton.nextElementSibling.remove();
+        completeButton.remove();
+        const backButton = document.createElement("button");
+        backButton.innerText = "戻す";
+        moveTarget.firstElementChild.appendChild(backButton);
+        document.getElementById("complete-list").appendChild(moveTarget);
+    });
+
+    // button(削除)タグ生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    deleteButton.addEventListener("click", () => {
+        // 押された削除ボタンの親にあるlliタグを未完了リストから削除
+        const deleteTarget = deleteButton.closest("li");
+        document.getElementById("incomplete-list").removeChild(deleteTarget);
+    });
+
     //階層構造
     li.appendChild(div);
     div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
 
     // 未完了リストに追加
     document.getElementById("incomplete-list").appendChild(li);
